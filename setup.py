58db8170527cef1feca2a0ca06b3ce5ef6dadb91
Nick Pegg <nick@nickpegg.com>
2011-12-07 14:57:15 -0600

    Added the required URL to setup.py. It's in PyPI now!

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from distutils.core import setup
 import ciscolib
 
 setup(name="ciscolib",
+    packages=['ciscolib'],
     version=ciscolib.__version__,
     description="Interacts with Cisco devices via command line",
     author="Nick Pegg",
     author_email="nick@nickpegg.com",
-    packages=['ciscolib'],
+    url="https://github.com/nickpegg/ciscolib",
     classifiers = [
         "Programming Language :: Python",
         "Development Status :: 3 - Alpha",
